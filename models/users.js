1626ce49047360ff58691b0ae3d244715d97ea33
Shaun Mascarenhas <shaunmascarenhas040@gmail.com>
2023-11-17 13:01:21 +0530

    first commit

---
+++ models/users.js
@@ -0,0 +1,19 @@
+// models/user.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('poop', userSchema);
